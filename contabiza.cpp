f9a11f66012971ffd62d4c13ba2acf0c337cb0c0
* <stefaniaocles21@gmail.com>
2022-09-27 18:30:52 -0500

    Programa contabiliza

---
+++ contabiza.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i=0,l;
+	float x,s=0;
+	cout<<"Ingrese el limite l=";cin>>l;
+	do{
+
+	cout<<"Ingrese el numero x="; cin>>x;
+	i=i+1;
+	s=s+x;
+
+	
+	}while(i<l);
+	cout<<"Se ingresaron "<<l<<"numeros"<<"que sumaron"<<s<<endl;
+	return 0;
