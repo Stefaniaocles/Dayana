12cc26be66515d462125b04a5bd616f147c73793
* <stefaniaocles21@gmail.com>
2022-09-28 17:07:17 -0500

    Modifique el programa velocimetro

--- velocimetro.cpp
+++ velocimetro.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-	float v,d,t;
+	float DSEO_ v,DSEO_ d,DSEO_ t;
 	cout<<"Ingrese la distancia(en km) d= "; cin>>d;
 	cout<<"Ingrese el tiempo(en horas) t= ";cin>>t;
 	v=d/t;
